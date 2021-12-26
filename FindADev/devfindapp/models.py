a01470c17ca9325b56451fe4736c771eb8ed03cf
yusufatalay <yusuftahaatalay14@gmail.com>
2021-12-26 20:06:37 +0300

    Building the backend, can get user's information from given language, but got rate limited even though I embed an Auth token

--- FindADev/devfindapp/models.py
+++ FindADev/devfindapp/models.py
@@ -6,13 +6,13 @@ class Keyword(models.Model):
 
 
 class User(models.Model):
-    user_id = models.IntegerField(unique= True)   # user_id is the unique identifier for the user's github profile
-    username = models.CharField(max_length=50)
-    profile_url = models.CharField(max_length=200)
-    email = models.EmailField(max_length=60,null= True) # null=True allows for blank email user may not have a publicly available email
-    name = models.CharField(max_length=50) 
-    bio = models.TextField(max_length=500,null=True) # null=True allows for blank bio user may not have a publicly available bio
-    query_id = models.ForeignKey(Keyword, verbose_name=("The related query for this user"), on_delete=models.CASCADE)
-    twitter_url = models.CharField(max_length=200,null=True)
-    location = models.CharField(max_length=50,null=True)
-    avatar_url = models.CharField(max_length=200,null=True)
+    user_id = models.IntegerField(unique= True)   # user_id is the unique identifier for the user's github profile                      # JSON->  id
+    username = models.CharField(max_length=50)  # username is the user's github username                                                # JSON->  login
+    profile_url = models.CharField(max_length=200) # profile_url is the user's github profile url                                       # JSON->  html_url   
+    email = models.EmailField(max_length=60,null= True) # null=True allows for blank email user may not have a publicly available email # JSON->  email
+    name = models.CharField(max_length=50)  # name is the user's full name                                                              # JSON->  name
+    bio = models.TextField(max_length=500,null=True) # null=True allows for blank bio user may not have a publicly available bio        # JSON->  bio
+    query_id = models.ForeignKey(Keyword, verbose_name=("The related query for this user"), on_delete=models.CASCADE) # query_id is the id of the query that generated this user
+    twitter_url = models.CharField(max_length=200,null=True) # twitter_url is the user's twitter profile url                            # JSON->  twitter_username
+    location = models.CharField(max_length=50,null=True) # location is the user's location                                              # JSON->  location
+    avatar_url = models.CharField(max_length=200,null=True) # avatar_url is the user's avatar url                                       # JSON->  avatar_url
--- FindADev/devfindapp/views.py
+++ FindADev/devfindapp/views.py
@@ -1,3 +1,44 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+SEARCH_URL = 'https://api.github.com/search/users?q=language:python' # My plan is, basically search the users by the appropriate language, and then display the results.
+ 
+def search(request):
+
+  params = {
+      "Authorization": 'token ' + GITHUB_AUTH_TOKEN,
+      'accept': 'application/vnd.github.v3+json',# adding this header to the GET request is recommended by the API documentation 
+      'per_page': '1',
+        'page': '1',
+  }
+  user_list = requests.get(SEARCH_URL, params=params); # this request returns a list of users, which we can then iterate through to display the results.
+  iuser_list = user_list.json()
+  usrs_urls = [] # will store relevant profile urls for each user, this urls are another API endpoints, which we can use to get the user's profile information.
+  for user in iuser_list['items']:
+    usrs_urls.append(user['url'])
+  users = []
+
+# get relevant user information from each user's profile url
+  for url in usrs_urls:
+    user_info = requests.get(url, params=params)
+    user_info = user_info.json()
+    users.append(user_info)
+
+  print(users[0])
+  ans = requests.get('https://api.github.com/rate_limit')
+  print (ans.json()['resources']['core']['remaining'])
+  print (ans.json()['resources']['search']['remaining'])
+ # for user in users:
+ #   print('*'*50)
+ #   print(user['name'])
+ #   print(user['id'])
+ #   print(user['login'])
+ #   print(user['email'])
+ #   print(user['twitter_username'])
+ #   print(user['html_url'])
+ #   print(user['avatar_url'])
+ #   print(user['bio'])
+ #   print(user['location'])
+  return render(request, 'search/index.html')
+
